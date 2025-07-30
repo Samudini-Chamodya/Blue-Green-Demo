40f90863fe6fb246fbd6b9bdf4b74eccacde8568
Samudini Chamodya <samudinirupasinha@gmail.com>
2025-07-30 22:52:32 +0530

    Fix bat script paths for Jenkins

--- scripts/deplot-to-blue.bat
+++ scripts/deplot-to-blue.bat
@@ -1,4 +1,4 @@
 @echo off
-cd ..\blue
+cd ..\C:\Users\USER\Desktop\BlueGreenDemo\blue
 call npm install
 start /B cmd /c "set PORT=5000 && set APP_VERSION=%1 && node app.js"
--- scripts/deploy-to-green.bat
+++ scripts/deploy-to-green.bat
@@ -1,4 +1,4 @@
 @echo off
-cd ..\green
+cd ..\C:\Users\USER\Desktop\BlueGreenDemo\green
 call npm install
 start /B cmd /c "set PORT=5001 && set APP_VERSION=%1 && node app.js"
